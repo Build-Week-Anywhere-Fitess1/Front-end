eadf37dd6dd6f6816ba4299d35c0f6462674bc79
krboelter <krboelter@gmail.com>
2019-11-13 18:49:38 -0700

    student redux ready to connect

--- src/actions/student.js
+++ src/actions/student.js
@@ -1,15 +1,21 @@
 import axios from 'axios';
 
+export const FETCH_STUDENT_START = "FETCH_STUDENT_START";
+export const FETCH_STUDENT_SUCCESS = "FETCH_STUDENT_SUCCESS";
+export const FETCH_STUDENT_ERROR = "FETCH_STUDENT_ERROR";
+
 export function fetchStudents() {
     return dispatch => {
         dispatch({type: FETCH_STUDENT_START})
 
         axios.get(`https://lambda-anywhere-fitness.herokuapp.com/`)
             .then(res => {
-                console.log(res)
+                console.log("Success!", res)
+                dispatch({type: FETCH_STUDENT_SUCCESS, payload: res})
             })
             .catch(err => {
                 console.log(err)
+                dispatch({type: FETCH_STUDENT_ERROR, payload: err})
             })
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { reducer as studentReducer } from './reducers/student';
 import App from './App';
 
 const rootReducer = combineReducers({
-
+    student: studentReducer
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-    <Provider store={}>
+    <Provider store={store}>
         <Router>
             <App />
         </Router>
