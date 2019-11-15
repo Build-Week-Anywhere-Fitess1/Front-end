8bf12e45721cd3e545650b8a050a97755c3cf23a
Charlie <charles.j.montoya@gmail.com>
2019-11-15 09:32:50 -0500

    Redux Up and Rendering Course Initial State

--- src/components/classes/CardsList.js
+++ src/components/classes/CardsList.js
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+
 
 //START Charlie
 import { fetchClasses } from '../../actions/classes';
 import { connect } from 'react-redux';
+import ClassCard from './ClassCard';
+
 //END Charlie
 
 
-function CardsList(props) {
+const CardsList = props => {
+
+  console.log(props.classes.classes, 'State-Class-List-Component')
+
+  useEffect( () => {
+
+    fetchClasses()
+
+  }, [])
 
   return (
-    <div>
-      
-      <h1>Dashboard - Card List</h1>
+    <>
+      <div>
 
-      {/* {props.cards.map(cards => (
-        <div className="class-card" key={cards.id}>
-          <img
-            className="class-image"
-            src={cards.imageUrl}
-            alt={cards.name}
-          />
-          <Link to={`/Cardlist/${cards.id}`}>
-            <p>{cards.name}</p>
-          </Link>
+        <h1>Dashboard - Card List</h1>
+
+        <div style={{display:'flex', flexDirection:'row', }}>
+          
+          {props.classes.classes.map((item, index) => (
+                      <ClassCard key={index} class_details={item}/>
+                  ))}
         </div>
-      ))} */}
-    </div>
-  );
+
+      </div>
+    </>
+  )
 }
 
 const mapStateToProps = state => {
