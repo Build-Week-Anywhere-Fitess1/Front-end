6fffab3ff7705dac58673d29e74189f220e2c56e
krboelter <krboelter@gmail.com>
2019-11-14 19:20:09 -0700

    working on reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchClasses } from './actions/classes';
 import './App.css';
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.fetchClasses()
+  }, [])
+
   return (
     <div className="App">
       Hello Anytime Fitness!
+      {props.classes.map(item => (
+        <div>
+          {item.name}
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    classes: state.classes.classes
+  }
+}
+
+const mapDispatchToProps = {
+  fetchClasses
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/actions/student.js
+++ src/actions/student.js
@@ -1,21 +1,21 @@
-import api from '../utils/api';
+// import api from '../utils/api';
 
-export const FETCH_STUDENT_START = "FETCH_STUDENTS_START";
-export const FETCH_STUDENT_SUCCESS = "FETCH_STUDENTS_SUCCESS";
-export const FETCH_STUDENT_ERROR = "FETCH_STUDENTS_ERROR";
+// export const FETCH_STUDENT_START = "FETCH_STUDENTS_START";
+// export const FETCH_STUDENT_SUCCESS = "FETCH_STUDENTS_SUCCESS";
+// export const FETCH_STUDENT_ERROR = "FETCH_STUDENTS_ERROR";
 
-export function fetchStudents() {
-    return dispatch => {
-        dispatch({type: FETCH_STUDENT_START})
+// export function fetchStudents() {
+//     return dispatch => {
+//         dispatch({type: FETCH_STUDENT_START})
 
-        api().get(`/api`)
-            .then(res => {
-                console.log("Success!", res)
-                dispatch({type: FETCH_STUDENT_SUCCESS, payload: res})
-            })
-            .catch(err => {
-                console.log(err)
-                dispatch({type: FETCH_STUDENT_ERROR, payload: err})
-            })
-    }
-}
+//         api().get(`/api`)
+//             .then(res => {
+//                 console.log("Success!", res)
+//                 dispatch({type: FETCH_STUDENT_SUCCESS, payload: res})
+//             })
+//             .catch(err => {
+//                 console.log(err)
+//                 dispatch({type: FETCH_STUDENT_ERROR, payload: err})
+//             })
+//     }
+// }
